705bec9a03538ed2faf758c126abcf6a3f8442fc
sionel <sadb0101@naver.com>
2023-08-18 19:10:24 +0900

    Feat: 복사용 api 수정 해야함

---
+++ src/pages/api/comment/index.tsx
@@ -0,0 +1,20 @@
+import { Task } from "@/types";
+import { NextApiRequest, NextApiResponse } from "next";
+export type ResponseError = {
+  message: string;
+};
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Task | ResponseError>
+) => {
+  switch (req.method) {
+    case "POST":
+      console.log(req.body);
+      
+      break;
+    case "PUT":
+      break;
+  }
+};
+
+export default handler;
