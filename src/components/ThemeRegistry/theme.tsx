f8ad259ba7272875db0c8f3f49b92d68bde31764
sionel <sadb0101@naver.com>
2023-08-18 19:11:20 +0900

    WIP: 테마 고민...

--- src/components/ThemeRegistry/theme.tsx
+++ src/components/ThemeRegistry/theme.tsx
@@ -20,5 +20,20 @@ import { createTheme } from "@mui/material/styles";
 //   },
 // });
 
-const theme = createTheme();
+const theme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: "#FAF3E5", // 베이지 기본색
+  //     contrastText: "#333", // 대조되는 텍스트 색상
+  //   },
+  //   secondary: {
+  //     main: "#064420", // 조금 더 어두운 베이지 계열의 색상
+  //     contrastText: "#FAF3E0", // 대조되는 텍스트 색상
+  //   },
+  //   background: {
+  //     default: "#FFEEDD", // 연한 크림색으로 설정
+  //     paper: "#F5E7D9", // drawer의 배경색으로 사용될 색상
+  //   },
+  // },
+});
 export default theme;
