9c03ff164a62e2aa7eb74b43b38a803959e38cd1
최가오 <44205245+sionel@users.noreply.github.com>
2023-08-21 17:06:00 +0900

    Refactor: tsx로 잘못된거 수정 & db 컬렉션 이름 수정

--- src/pages/api/task/index.ts
+++ src/pages/api/task/index.ts
@@ -12,10 +12,10 @@ const handler = async (
     case "POST":
       const task: Task = { title: req.body.title, isCheck: false };
       const { db } = await connectToDatabase();
-      const result = await db.collection("Task").insertOne(task);
-      if(result.acknowledged){        
-        res.writeHead(302, { Location: '/tasks' }).end();
-      }else {
+      const result = await db.collection("tasks").insertOne(task);
+      if (result.acknowledged) {
+        res.writeHead(302, { Location: "/tasks" }).end();
+      } else {
         res.status(500).json({ message: "Internal Server Error" });
       }
     case "PUT":
