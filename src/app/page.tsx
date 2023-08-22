ca99a66c74bbb6d9bd2e71be3bf1ab627edacc2d
최가오 <44205245+sionel@users.noreply.github.com>
2023-08-22 17:03:21 +0900

    Feat: api 구조 변경 테스트중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
+import { connectToDatabase } from "@/util/database";
 import styles from "./page.module.css";
+import { cookies } from "next/headers";
 
-export default function Home() {
+// const postVisit = async () => {
+//   const cookiesList = cookies();
+//   const visited = cookiesList.has("visited");
+//   if (!visited) {
+//     let { db } = await connectToDatabase();
+//     await db
+//       .collection("visits")
+//       .updateOne({}, { $inc: { today: 1, total: 1 } })
+//       .then((e) => {
+//         const oneDay = 24 * 60 * 60 * 1000;
+//         cookies().set("visits", "true", { expires: Date.now() - oneDay });
+//       });
+//   }
+// };
+const Home = async () => {
+  // await postVisit();
   return (
     <div className={styles.main}>
-      <div className={styles.text}>
-        게시판을 가냥한 각각의 메뉴
-      </div>
+      <div className={styles.text}>게시판을 가냥한 각각의 메뉴</div>
     </div>
   );
-}
+};
+export default Home;
