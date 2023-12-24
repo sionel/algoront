4234c8eb197391d67bb1d71656a0efd630d80e48
sionel <sadb0101@naver.com>
2023-12-24 11:07:35 +0900

    Feat : 글로벌 collection 생성

--- src/util/database/index.ts
+++ src/util/database/index.ts
@@ -1,5 +1,5 @@
 import { ApiData, ClientData } from "@/types";
-import { MongoClient, Db } from "mongodb";
+import { MongoClient, Db, Collection } from "mongodb";
 
 const url = process.env.DATABASE_URI as string;
 const dbName = process.env.DATABASE_NAME as string;
@@ -22,11 +22,39 @@ export const connectToDatabase = async () => {
       db: db,
     };
 
+    setCollection(db);
+
     return cachedClient;
   } catch (err) {
     console.error("MongoDB 연결 오류:", err);
     throw err;
   }
+};
+
+const setCollection = (db: Db) => {
+  const commentsName = process.env.COMMENTS_COLLECTION_NAME ?? "";
+  const markdownsName = process.env.MARKDOWNS_COLLECTION_NAME ?? "";
+  const postsName = process.env.POSTS_COLLECTION_NAME ?? "";
+  const questionsName = process.env.QUESTIONS_COLLECTION_NAME ?? "";
+  const tasksName = process.env.TASKS_COLLECTION_NAME ?? "";
+  const testcasesName = process.env.TESTCASES_COLLECTION_NAME ?? "";
+  const visitName = process.env.VISIT_COLLECTION_NAME ?? "";
+
+  const commentsCollection: Collection = db.collection(commentsName);
+  const markdownsCollection: Collection = db.collection(markdownsName);
+  const postsCollection: Collection = db.collection(postsName);
+  const questionsCollection: Collection = db.collection(questionsName);
+  const tasksCollection: Collection = db.collection(tasksName);
+  const testcasesCollection: Collection = db.collection(testcasesName);
+  const visitCollection: Collection = db.collection(visitName);
+
+  collections.comments = commentsCollection;
+  collections.markdowns = markdownsCollection;
+  collections.posts = postsCollection;
+  collections.questions = questionsCollection;
+  collections.tasks = tasksCollection;
+  collections.testcases = testcasesCollection;
+  collections.visit = visitCollection;
 };
 
 export const convertApiDataToClientData = <T>(
@@ -35,3 +63,13 @@ export const convertApiDataToClientData = <T>(
   const { _id, ...rest } = apiData;
   return { ...rest, id: String(_id) };
 };
+
+export const collections: {
+  comments?: Collection;
+  markdowns?: Collection;
+  posts?: Collection;
+  questions?: Collection;
+  tasks?: Collection;
+  testcases?: Collection;
+  visit?: Collection;
+} = {};
